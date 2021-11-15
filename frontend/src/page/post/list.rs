813887b2258cab98659a74e8559be04a48cd0050
Songday <songday@yeah.net>
2021-11-15 13:57:52 +0800

    Create list.rs

--- frontend/src/page/post/list.rs
+++ frontend/src/page/post/list.rs
@@ -18,20 +18,24 @@ impl Component for PostList {
         Self
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
             Msg::Compose => {
                 wasm_bindgen_futures::spawn_local(async move {
                     let response = reqwasm::http::Request::get("/post/new").send().await.unwrap();
                     let json: Response<u64> = response.json().await.unwrap();
                     if json.status == 0 {
-                        yew_router::push_route(crate::router::Route::ComposePost { id: json.data.unwrap() });
+                        ctx.link().history().unwrap().push_route(crate::router::Route::ComposePost { id: json.data.unwrap() }).unwrap();
+                        // yew_router::push_route(crate::router::Route::ComposePost { id: json.data.unwrap() });
                     } else {
+                        ctx.link().location().unwrap().set_href("/management");
+                        /*
                         if let Some(loc) = web_sys::window().map(|window| window.location()) {
                             let _ = loc.set_href("/management");
                         } else {
                             console_log!("get location failed");
                         }
+                        */
                     }
                 });
             }
