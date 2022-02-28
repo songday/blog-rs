a31cc017e0b022739d211e43b678abc58f1fc971
Songday <songday@yeah.net>
2022-02-28 14:43:35 +0800

    Release v0.5.5

--- frontend/dist/snippets/blog-frontend-bec5c12e8e7a4cdd/asset/show.js
+++
@@ -1,9 +0,0 @@
-export function showNotificationBox() {
-    document.getElementById('notification').style.display = 'block';
-}
-export function hideNotificationBox(event) {
-    let source = event.target || event.srcElement;
-    while (source.id !== 'notification' && source.parentNode)
-        source = source.parentNode;
-    source.style.display = 'none';
-}
